71c950fee5e4d79d175d38fbae7f253433f32732
akatsukydeliverysushi-coder <akatsuky.deliverysushi@gmail.com>
2026-08-30 09:30:27 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,55 +1,14 @@
-const C = "akatsuky-v5-5";
-
-const FILES = [
-  "./",
-  "./index.html",
-  "./admin.html",
-  "./style.css",
-  "./app.js",
-  "./admin.js",
-  "./firebase-config.js",
-  "./manifest.json",
-  "./logo.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(C)
-      .then(cache => cache.addAll(FILES))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys =>
-        Promise.all(
-          keys
-            .filter(key => key !== C)
-            .map(key => caches.delete(key))
-        )
-      )
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-
-        caches.open(C).then(cache => {
-          cache.put(event.request, copy);
-        });
-
-        return response;
-      })
-      .catch(() =>
-        caches.match(event.request).then(response => response)
-      )
-  );
+const CACHE_NAME="akatsuky-v5-5";
+const PRECACHE=["./","./index.html","./admin.html","./style.css","./app.js","./admin.js","./firebase-config.js","./manifest.json","./logo.png","./troco.js","./impressao-troco.js"];
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  event.respondWith(fetch(event.request).then(response=>{
+    if(response && response.ok){
+      const copy=response.clone();
+      caches.open(CACHE_NAME).then(c=>c.put(event.request,copy));
+    }
+    return response;
+  }).catch(()=>caches.match(event.request)));
 });
