ad65fb21d6cef8ac0bd2bd3ad457f378b358647f
akatsukydeliverysushi-coder <akatsuky.deliverysushi@gmail.com>
2026-08-30 11:16:42 -0300

    Atualizar service worker e cache do aplicativo

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-const CACHE='akatsuky-v6';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./app.js']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='akatsuky-v8';
+const APP_SHELL=['./','./index.html','./style.css','./app.js','./install.js','./manifest.json','./firebase-config.js','./delivery-rules.js','./logo.png'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL).catch(()=>{})));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(
+    fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+      return response;
+    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
+  );
+});
