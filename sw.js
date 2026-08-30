03984422e974e3d646118c77baa9ad3503d3e525
akatsukydeliverysushi-coder <akatsuky.deliverysushi@gmail.com>
2026-08-30 09:24:35 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,55 @@
-const C="akatsuky-v3-4";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","index.html","admin.html","style.css","app.js","admin.js","firebase-config.js","manifest.json","logo.png"]))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C = "akatsuky-v5-5";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./admin.html",
+  "./style.css",
+  "./app.js",
+  "./admin.js",
+  "./firebase-config.js",
+  "./manifest.json",
+  "./logo.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(C)
+      .then(cache => cache.addAll(FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== C)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(C).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then(response => response)
+      )
+  );
+});
